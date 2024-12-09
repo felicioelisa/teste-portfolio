a297a101c734d25b79fa5988541c2abeb2ac75c9
Elisa Felicio <felicioelisa@gmail.com>
2024-12-09 17:39:22 -0300

    tela de experiencias

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,8 +1,22 @@
+import Image from "next/image";
+import work from "../../../public/work.jpg"
+import "./index.css";
+import { Col, Container, Row } from "react-bootstrap";
+
 export default function Jobs() {
     return (
-        <div>
-            Qualitor
-            Solucap
-        </div>
+        <main>
+            <Container fluid="md">
+                <Row xs={1} md={4}>
+                    <Col>
+                        <Image src={work} width={0} height={0} alt="logo" />
+                    </Col>
+                    <Col>Solucap</Col>
+                    <Col>Qualitor</Col>
+                    <Col>SENAI - ISI SIM</Col>
+                    <Col>Banrisul</Col>
+                </Row>
+            </Container>           
+		</main>
     );
 }
